dc02e5e353b5f0dbcbdcf3f403574f5033bb0a09
Jake Kim <imjakekim@gmail.com>
2022-09-12 10:39:43 -0700

    finished extension

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,16 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+
+  def rides_by_name
+    rides.order(:name)
+  end
+
+  def avg_thrill
+    total_thrill = 0.0
+    rides.each do |ride|
+      total_thrill += ride.thrill_rating
+    end
+
+    total_thrill / rides.length
+  end
 end
