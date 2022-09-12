7daa607c74d9c0a7a7520fa779459b0149f21865
Jake Kim <imjakekim@gmail.com>
2022-09-12 08:21:43 -0700

    created model relationship tests

---
+++ spec/models/mechanic_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Mechanic, type: :model do
+  describe 'relationships' do
+    it { should have_many(:ride_mechs)}
+    it { should have_many(:rides).through(ride_mechs) }
+  end
+end
